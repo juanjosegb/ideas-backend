676a2a8b42f320abedbdd08bfbe1760e918ebb6f
juanjosegb <juanjose.garciabeza@hotmail.com>
2020-11-25 13:51:40 +0100

    [FIX] Time line ideas + User ideas

--- ideas/querys.py
+++ ideas/querys.py
@@ -22,12 +22,12 @@ class Query(graphene.AbstractType):
         follower = info.context.user
         isFollowed = Follow.objects.filter(follower=follower, followed=user_id, pending_request=False, approved=True).exists()
         if isFollowed:
-            return Idea.objects.filter(Q(creator=user_id) | ~Q(visibility="PRIVATE")).order_by('created_at')
+            return Idea.objects.filter(Q(creator=user_id), ~Q(visibility="PRIVATE")).order_by('created_at')
         else:
-            return Idea.objects.filter(Q(creator=user_id, visibility="PUBLIC")).order_by('created_at')
+            return Idea.objects.filter(Q(creator=user_id), Q(visibility="PUBLIC")).order_by('created_at')
 
     @login_required
     def resolve_timeline_ideas(self, info):
         user = info.context.user
-        followedUsers = Follow.objects.filter(follower=user, pending_request=False, approved=True).values_list("followed")
-        return Idea.objects.filter(Q(creator=user), Q(creator=followedUsers) | ~Q(visibility="PRIVATE")).order_by('created_at')
+        followed = Follow.objects.filter(follower=user, pending_request=False, approved=True).values_list("followed")
+        return Idea.objects.filter(Q(creator=user) | Q(creator=followed), ~Q(visibility="PRIVATE")).order_by('created_at')
